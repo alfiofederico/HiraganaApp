cf840e0423d5f436181f425a155227d96b4ad308
Alfio Federico Di Pinto <fefferico81@gmail.com>
2025-05-20 10:30:22 +0900

    service worker

--- service-worker.js
+++ service-worker.js
@@ -16,6 +16,20 @@ self.addEventListener("install", (event) => {
   );
 });
 
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
