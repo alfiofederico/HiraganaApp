7205b699610164b04e42257f1fde45aff49e5fa0
Alfio Federico Di Pinto <fefferico81@gmail.com>
2025-05-20 09:41:41 +0900

    applify

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+// service-worker.js
+const CACHE_NAME = "hiragana-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./hiragana_puzzles.json",
+  "./icon-192.png",
+  "./icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
